d72273a6743b5c2d5c34ea43ee083109e550e360
Breno <breno.cn@hotmail.com>
2021-04-30 21:20:21 -0300

    primeira versão de TECAleatorio, já possui gerarTempo()

--- TEC/TECAleatorio.py
+++ TEC/TECAleatorio.py
@@ -1,11 +1,50 @@
-import pandas as pd
+import numpy as np
 
 class TECAleatorio:
 
     # Recebe o caminho para um csv contendo dados iniciais
     # que serão usados para o método Monte Carlo
-    def __init__(self, dadosIniciais: str):
-        self.data = pd.read_csv(dadosIniciais)
+    def __init__(self, filepath, intervalos):
+        self.dadosIniciais = []
+        self.dadosIniciaisIntervalados = []
+        self.intervalos = intervalos
+        self.pontosMedios = []
+        self.menor = 0
+        self.maior = 0
 
-    def gerarTempo():
-        pass
+        with open(filepath) as dados:
+            linhas = dados.read().splitlines()
+            self.dadosIniciais = np.array(list(map(int, linhas)))
+            self.dadosIniciais.sort()
+            self.dadosIniciaisIntervalados = np.array_split(self.dadosIniciais, intervalos)
+
+            self.pontosMedios = [intervalo.mean() for intervalo in self.dadosIniciaisIntervalados]
+
+            self.menor = self.dadosIniciais[0]
+            self.maior = self.dadosIniciais[-1]
+
+            print(self.intervalos)
+            print(self.dadosIniciais)
+            print(self.dadosIniciaisIntervalados)
+            print(self.pontosMedios)
+
+    def gerarTempo(self):
+        # Gerar um numero aleatorio
+        num = np.random.randint(low=self.menor, high=self.maior, size=1)[0]
+        print(f'numero sorteado: {num}')
+
+        # Verificar qual intervalo esse número pertence
+        i = 0
+        for intervalo in self.dadosIniciaisIntervalados:
+            menor = intervalo[0]
+            maior = intervalo[-1]
+
+            # intervalo encontrado
+            if num >= menor and num <= maior:
+                # print('AQUI')
+                # print(f'intervalo = {intervalo}')
+                return self.pontosMedios[i]
+
+            i += 1
+
+        # return self.pontosMedios[-1]
--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from TEC.TECAleatorio import *
+
 def main():
-    pass
+    TEC = TECAleatorio('tempo_chegada_teste.txt', 2)
+    temp1 = TEC.gerarTempo()
+    temp2 = TEC.gerarTempo()
+    temp3 = TEC.gerarTempo()
+    temp4 = TEC.gerarTempo()
+
+    print(temp1)
+    print(temp2)
+    print(temp3)
+    print(temp4)
+
 
 if __name__ == '__main__':
     main()
